0f43ec6ea433295ddd76e3dd5035f5a0417627a4
Bennet <46792326+BennetE@users.noreply.github.com>
2021-12-03 00:09:57 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,21 +3,13 @@ const path = require("path");
 require("dotenv")
     .config();
 
-const app = express();
-
 const clientCache = process.env.CLIENT_CACHE_DURATION ?? "300";
 const cdnCache = process.env.CDN_CACHE_DURATION ?? "600"
 
+const app = express();
 app.use((req, res, next) => {
     res.set({ "Cache-Control": `public, max-age=${clientCache}, s-maxage=${cdnCache}` });
     next();
 });
 app.use("/docs", express.static(path.join(__dirname, "build")));
-app.get("/test", (req, res) => {
-    res.json({
-        message: "hi",
-        clientCache,
-        cdnCache
-    });
-});
 app.listen(8080);
